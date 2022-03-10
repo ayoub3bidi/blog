476f02e45b4cf3dd8a909be99b9f1fe25dd8e918
ayoub3bidi <mrayoubabidi@gmail.com>
2022-03-10 19:14:28 +0100

    Adding Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <Home />
-      </header>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
+          <Switch>
+            <Route path="/">
+              <Home />  
+            </Route>
+          </Switch>
+        </header>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
