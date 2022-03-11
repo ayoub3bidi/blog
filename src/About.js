212077b562fe443486d2e4c319b1e5951f67b808
ayoub3bidi <mrayoubabidi@gmail.com>
2022-03-11 20:03:11 +0100

    About page edit

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
 const About = () => {
     return ( 
-        <div className="about">
-            <h3>This is the about page</h3>    
+        <div className="home">
+            <h2 className="about">Hello my name is Ayoub Abidi and this is a simple blog i made with React ❤</h2>    
         </div>
      );
 }
